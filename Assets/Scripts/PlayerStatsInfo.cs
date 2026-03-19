775b4d307a6ad07b865e368e40d3289a1bce0e15
David <david.lleida7@gmail.com>
2026-03-19 11:40:08 +0100

    Show stat difference on Stat info

--- Assets/Scripts/PlayerStatsInfo.cs
+++ Assets/Scripts/PlayerStatsInfo.cs
@@ -8,7 +8,20 @@ public class PlayerStatsInfo : MonoBehaviour
 	public Text coinMultiplierNumber;
 	public Text healthNumber;
 
-	private void Start()
+	private float health = 60;
+	private float damage = 1.2f;
+	private float passiveDamage = 0;
+	private float coinMultiplier = 1;
+
+	private void Awake()
+	{
+		health = PlayerStats.Instance.health;
+		damage = PlayerStats.Instance.damage;
+		passiveDamage = PlayerStats.Instance.passiveDamage;
+		coinMultiplier = PlayerStats.Instance.coinMultiplier;
+	}
+
+	private void OnEnable()
 	{
 		PrintStatsValues();
 	}
@@ -20,9 +33,28 @@ public class PlayerStatsInfo : MonoBehaviour
 
 	private void PrintStatsValues()
 	{
-		damageNumber.text = PlayerStats.Instance.damage.ToString();
-		pasiveDamageNumber.text = PlayerStats.Instance.passiveDamage.ToString();
-		coinMultiplierNumber.text = PlayerStats.Instance.coinMultiplier.ToString();
-		healthNumber.text = PlayerStats.Instance.health.ToString();
+		damageNumber.text = PlayerStats.Instance.damage.ToString("F2") + StatVariability(damage, PlayerStats.Instance.damage);
+		pasiveDamageNumber.text = PlayerStats.Instance.passiveDamage.ToString("F2") + StatVariability(passiveDamage, PlayerStats.Instance.passiveDamage); ;
+		coinMultiplierNumber.text = PlayerStats.Instance.coinMultiplier.ToString("F2")  + StatVariability(coinMultiplier, PlayerStats.Instance.coinMultiplier); ;
+		healthNumber.text = PlayerStats.Instance.health.ToString("F2") + StatVariability(health, PlayerStats.Instance.health); ;
+	}
+
+	private string StatVariability(float oldStatValue, float newStatValue)
+	{
+		float amount = newStatValue - oldStatValue;
+		float sign = oldStatValue - newStatValue;
+
+		if (sign < 0)
+		{
+			return "<color=green> +" + amount.ToString("F2") + "</color>";
+		}
+		else if (sign > 0)
+		{
+			return "<color=red> " + amount.ToString("F2") + "</color>";
+		}
+		else
+		{
+			return "";
+		}
 	}
 }
